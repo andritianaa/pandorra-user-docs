df3252c3449a91f708ba1c53584117e3d8031c1b
Rakotonimanana T. Andritiana Steve <andritianasteve@gmail.com>
2024-09-13 13:54:17 +0300

    Help center first commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,40 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  logo: (
+    <div
+      className="logo"
+      style={{
+        display: "flex",
+        gap: "0.5rem",
+        padding: "6px",
+        alignItems: "center",
+      }}
+    >
+      <img
+        src="/logo-base-256x256.png"
+        style={{ width: "2rem", height: "2rem" }}
+      />
+      <p style={{ fontSize: "1.5rem" }}>Pandorra.ai - Help Center</p>
+    </div>
+  ),
   footer: {
-    text: 'Nextra Docs Template',
+    text: "Pandorra.ai help center (All right reserved 2024)",
   },
-}
+  editLink: {
+    component: ({ children, className, filePath }) => (
+      <a href={filePath} className={className}>
+        {children}
+      </a>
+    ),
+    text: "",
+  },
+  feedback: {
+    content: "", // Peut être un texte ou un élément ReactNode
+    labels: "", // Un label pour décrire le feedback
+    useLink: (page) => `/feedback?source=${page}`, // Une fonction pour génér
+  },
+};
 
-export default config
+export default config;
